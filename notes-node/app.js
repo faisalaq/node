67109868550b3283a7b4e2d11548062600ed19fc
faisalaq <faq0818@gmail.com>
2018-03-22 14:46:13 -0700

    edited app.js

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,11 +1,14 @@
 console.log("Starting app.js");
 
 const fs = require('fs');
-const os = require('os');
 const notes = require('./notes.js');
 const _ = require('lodash');
+
 console.log(_.isString(true));
 console.log(_.isString('true'));
+
+console.log(_.uniq(['Faisal', 1,2,3,4,2,1,'fajlu']));
+
 // var user = os.userInfo();
 // var add = notes.add(9,3)
 // console.log(user.username);
